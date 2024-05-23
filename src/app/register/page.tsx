e12b6257105765cd8d2994a198ebcb077fed9259
ericcui <ericcui@gmail.com>
2024-05-23 12:48:41 -0700

    Completed basic functionality of register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -16,9 +16,11 @@ export default function RegisterPage() {
     }
 
     // Form submission handling
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmitUser = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const response = await fetch('/api/new', { // API call
+
+        // Creating a new user
+        const response = await fetch('/api/new', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -29,20 +31,39 @@ export default function RegisterPage() {
         })
         console.log(response.json());
     }
+
+    const handleSubmitHabit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const response = await fetch('/api/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: userValue,
+                habit: habitValue,
+            })
+        });
+        console.log(response.json());
+    }
     return (
         <div className="flex flex-col justify-center items-center">
-            <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-4 mt-4">
+            <form onSubmit={handleSubmitUser} className="flex flex-col justify-center items-center gap-4 mt-4">
                 <div>
                     <label>
                         <input placeholder="Username" className="text-black ml-2 pl-1 rounded-md" type="text" onChange={handleInputChange} value={userValue}/>
                     </label>
                 </div>
+                <button type="submit" className="border border-solid border-white w-20 rounded bg-blue-400 font-bold text-center">New User</button>
+            </form>
+            <form onSubmit={handleSubmitHabit} className="flex flex-col justify-center items-center gap-4 mt-4">
                 <div>
                     <label>
                         <input placeholder="Habit" className="text-black ml-2 pl-1 rounded-md" type="text" onChange={handleInputChange} value={habitValue}/>
                     </label>
                 </div>
-                <button type="submit" className="border border-solid border-white w-20 rounded bg-blue-400 font-bold text-center">Submit</button>
+                <button type="submit" className="border border-solid border-white w-20 rounded bg-blue-400 font-bold text-center">Add Habit</button>
             </form>
         </div>
     )
